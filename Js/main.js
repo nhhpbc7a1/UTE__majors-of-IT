74efa5176d35ea01e2b130f16feb26891cabca1f
Nguyễn Hoàng Huy <skill0sp1@gmail.com>
2022-12-18 15:51:10 +0700

    delete no need folder

--- Js/main.js
+++ Js/main.js
@@ -11,19 +11,31 @@ window.onscroll = function () {
 }; 
 // <=== Syntax Error: Closing Parenthese around an if block
 var cuFrame=1;
-function go(id){
+function header_go(id){
+    console.log(cuFrame);
+
     if(id=='2'&&cuFrame>1){cuFrame--;}else{if(id=='2')cuFrame=9;}
 
     if(id=='1'&&cuFrame<9){cuFrame++;}else{if(id=='1')cuFrame=1;}
 
-    window.location.href = '#slide' + cuFrame;
-
+    window.location.href = '#header_slide' + cuFrame;
 }
-var cuFrame2=1;
-function goMem(id){
-    if(id=='2'&&cuFrame2>1){cuFrame2--;}else{if(id=='2')cuFrame2=6;}
 
-    if(id=='1'&&cuFrame2<6){cuFrame2++;}else{if(id=='1')cuFrame2=1;}
+var cuFrame_reader=1;
+function reader_go(id){
+    console.log(cuFrame_reader);
 
-    window.location.href = '#mem' + cuFrame2;
+    if(id=='2'&&cuFrame_reader>1){cuFrame_reader--;}else{if(id=='2')cuFrame_reader=3;}
+
+    if(id=='1'&&cuFrame_reader<3){cuFrame_reader++;}else{if(id=='1')cuFrame_reader=1;}
+
+    window.location.href = '#reader_slide' + cuFrame_reader;
+}
+var cuFrame_mem=1;
+function mem_go(id){
+    if(id=='2'&&cuFrame_mem>1){cuFrame_mem--;}else{if(id=='2')cuFrame_mem=6;}
+
+    if(id=='1'&&cuFrame_mem<6){cuFrame_mem++;}else{if(id=='1')cuFrame_mem=1;}
+
+    window.location.href = '#mem_slide' + cuFrame_mem;
 }
