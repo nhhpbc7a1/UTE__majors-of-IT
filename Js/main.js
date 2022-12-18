40bca4000eb6a85f249263987b7e365695f9d2a4
Nguyễn Hoàng Huy <skill0sp1@gmail.com>
2022-12-18 13:36:29 +0700

    update_font

--- Js/main.js
+++ Js/main.js
@@ -1,14 +1,6 @@
 
-window.onscroll = function () {
-    if ($(window).scrollTop() == 0) {
-        console.log(1);
-        $(".header").addClass("trans")
-    }
-    else {
-        console.log(0);
-        $(".header").removeClass("trans")
-    }
-}; // <=== Syntax Error: Closing Parenthese around an if block
+
+// <=== Syntax Error: Closing Parenthese around an if block
 var cuFrame=1;
 function go(id){
     if(id=='2'&&cuFrame>1){cuFrame--;}else{if(id=='2')cuFrame=9;}
@@ -26,3 +18,13 @@ function goMem(id){
 
     window.location.href = '#mem' + cuFrame2;
 }
+window.onscroll = function () {
+    if ($(window).scrollTop() == 0) {
+        console.log(1);
+        $(".header").addClass("trans")
+    }
+    else {
+        console.log(0);
+        $(".header").removeClass("trans")
+    }
+}; 
